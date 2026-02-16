062e42a55d12b6cb6ed114a14cda053fa505936d
EmmaThinks <em4xds@proton.me>
2026-02-15 20:02:41 -0600

    removed target folder from daemon project

--- projects/login_sys/target/debug/deps/login_sys-6e5def7d9a0f9026.d
+++
@@ -1,5 +0,0 @@
-/home/emma/Documents/Rust/login_sys/target/debug/deps/login_sys-6e5def7d9a0f9026.d: src/main.rs
-
-/home/emma/Documents/Rust/login_sys/target/debug/deps/liblogin_sys-6e5def7d9a0f9026.rmeta: src/main.rs
-
-src/main.rs:
--- projects/login_sys/target/debug/login_sys.d
+++
@@ -1,1 +0,0 @@
-/home/emma/Documents/Rust/login_sys/target/debug/login_sys: /home/emma/Documents/Rust/login_sys/src/main.rs
