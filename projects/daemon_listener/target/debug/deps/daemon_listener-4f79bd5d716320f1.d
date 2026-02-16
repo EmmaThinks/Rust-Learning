2dfb10f8d99ff6efaa9f0e6e8299954c2f0b2a2f
EmmaThinks <em4xds@proton.me>
2026-02-15 19:57:46 -0600

    removed target folder from daemon project

--- projects/daemon_listener/target/debug/deps/daemon_listener-4f79bd5d716320f1.d
+++
@@ -1,5 +0,0 @@
-/home/emma/Documents/Rust-Learning/projects/daemon_listener/target/debug/deps/daemon_listener-4f79bd5d716320f1.d: src/main.rs
-
-/home/emma/Documents/Rust-Learning/projects/daemon_listener/target/debug/deps/daemon_listener-4f79bd5d716320f1: src/main.rs
-
-src/main.rs:
--- projects/daemon_listener/target/debug/deps/daemon_listener-c9123a660ed58998.d
+++
@@ -1,5 +0,0 @@
-/home/emma/Documents/Rust-Learning/projects/daemon_listener/target/debug/deps/daemon_listener-c9123a660ed58998.d: src/main.rs
-
-/home/emma/Documents/Rust-Learning/projects/daemon_listener/target/debug/deps/libdaemon_listener-c9123a660ed58998.rmeta: src/main.rs
-
-src/main.rs:
